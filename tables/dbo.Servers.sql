f04a537d8c020977c9d2b7814ed34fe3166057d2
Andy Mallon <amtwo@users.noreply.github.com>
2021-04-09 09:40:01 -0400

    Create dbo.Servers.sql

---
+++ tables/dbo.Servers.sql
@@ -0,0 +1,9 @@
+IF ( object_id('dbo.Servers','U') IS NULL )
+BEGIN
+    CREATE TABLE dbo.Servers (
+        ServerName      nvarchar(128)   NOT NULL,
+        ServerFQDN      nvarchar(128)   NOT NULL,
+        Environment     varchar(16)     NULL
+        CONSTRAINT PK_WindowsServers PRIMARY KEY CLUSTERED (ServerName)
+        );
+END;
