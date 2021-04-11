16750c55700eefed4af482a4b52c491538443a02
Andy Mallon <amtwo@users.noreply.github.com>
2021-04-11 18:01:55 -0400

    Update dbo.EstateServers.sql
    Add license metadata to dbo.EstateServers

--- tables/dbo.EstateServers.sql
+++ tables/dbo.EstateServers.sql
@@ -3,7 +3,9 @@ BEGIN
     CREATE TABLE dbo.EstateServers (
         ServerName      nvarchar(128)   NOT NULL,
         ServerFQDN      nvarchar(128)   NOT NULL,
-        Environment     varchar(16)     NULL
+        Environment     varchar(16)     NULL,
+        SqlLicenseType  varchar(16)     NULL,
+        SqlSaPrimary    nvarchar(128)   NULL
         CONSTRAINT PK_EstateServers PRIMARY KEY CLUSTERED (ServerName)
         );
 END;
