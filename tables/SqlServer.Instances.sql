c216c9f4c741bdbea8d6c724e82f18e72c4ca218
Andy Mallon <amtwo@users.noreply.github.com>
2021-04-11 21:15:13 -0400

    Update SqlServer.Instances.sql

--- tables/SqlServer.Instances.sql
+++ tables/SqlServer.Instances.sql
@@ -1,9 +1,11 @@
 IF ( object_id('SqlServer.Instances','U') IS NULL )
 BEGIN
     CREATE TABLE SqlServer.Instances (
-        ServerName      nvarchar(128)   NOT NULL,
-        InstanceName    nvarchar(128)   NOT NULL,
-        Purpose         varchar(50)     NULL
+        ServerName        nvarchar(128)   NOT NULL,
+        InstanceName      nvarchar(128)   NOT NULL,
+        SqlEdition        varchar(128)    NULL,
+        SqlProductVersion varchar(16)     NULL,
+        Purpose           varchar(50)     NULL
         CONSTRAINT PK_SqlInstances PRIMARY KEY CLUSTERED (ServerName,InstanceName)
         );
 END;
